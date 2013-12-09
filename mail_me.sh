6443a89b5c116b5ff3097ccc1e6862f834f69457
iznam <i-znam@ya.ru>
2013-12-09 16:32:44 +0300

    Update mail_me.sh

--- mail_me.sh
+++ mail_me.sh
@@ -2,10 +2,11 @@
 
 #dhclient -v eth0
 
+mail_addr=
 ip=$(ifconfig eth0 | awk -F"addr:" '/inet addr/{print $2}'|sed 's/ Bcast.*//')
 ipmi=$(ipmitool lan print | grep "IP Address" | grep -v "Source"| sed 's/IP Address.*://')
-echo "Hi, this is a mail from $HOSTNAME server [nVidia K20]
+echo "Hi, this is a mail from $HOSTNAME server 
 
 Server has rebooted few minutes ago. 
 Now my IP is:  $ip
-My IPMI ip is: $ipmi" | mail -s "Server \"$HOSTNAME\" changed IP" iznam@yandex-team.ru
+My IPMI ip is: $ipmi" | mail -s "Server \"$HOSTNAME\" changed IP" $mail_addr
