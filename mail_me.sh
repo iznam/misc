9169cf7a262eb793912c6d4b53f687b8e4b9561d
iznam <i-znam@ya.ru>
2013-12-09 16:23:33 +0300

    Update mail_me.sh

--- mail_me.sh
+++ mail_me.sh
@@ -4,7 +4,7 @@
 
 ip=$(ifconfig eth0 | awk -F"addr:" '/inet addr/{print $2}'|sed 's/ Bcast.*//')
 ipmi=$(ipmitool lan print | grep "IP Address" | grep -v "Source"| sed 's/IP Address.*://')
-echo "Hi, this is a mail from GPU server [nVidia K20]
+echo "Hi, this is a mail from $HOSTNAME server [nVidia K20]
 
 Server has rebooted few minutes ago. 
 Now my IP is:  $ip
